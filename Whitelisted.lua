16a4c13908f98686b7f072d1585af12e5281a8ff
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 12:50:43 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -1,3 +1,4 @@
+-- Whitelist
 local whitelist = {
     ["ZENO_HUB|157|155|157|153|147|167|162|153|156"] = true,
     ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true,
@@ -17,4 +18,20 @@ if isWhitelisted(keyToCheck) then
     print("Key is whitelisted")
 else
     print("Key is not whitelisted")
+end
+
+-- Check if the player is whitelisted based on their UserId
+local function isPlayerWhitelisted(player)
+    return player.UserId and whitelist["ZENO_HUB|" .. tostring(player.UserId)] == true
+end
+
+-- Get the player
+local player = game.Players.LocalPlayer
+
+-- Check if the player is whitelisted
+if player and isPlayerWhitelisted(player) then
+    print("Player is whitelisted")
+else
+    print("Player is not whitelisted")
+    -- Optional: You can kick or handle blacklisted players here
 end
