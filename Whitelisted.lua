287caa09b3cab469d2568644aa09d38555cfdce4
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 14:16:34 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -1,26 +1,18 @@
 -- Load the whitelist
 local whitelist = {
-    "ZENO_HUB|157|155|157|153|147|167|162|153|156",
-    "ZENO_HUB|149|154|148|160|153|172|157|154|155|149",
-    "ZENO_HUB|149|154|148|160|153|172|157|154|155|149",
-    "ZENO_HUB|151|153|151|160|146|171|155|157|160|156"
+    ["ZENO_HUB|157|155|157|153|147|167|162|153|156"] = true,
+    ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true,
+    ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true,
+    ["ZENO_HUB|151|153|151|160|146|171|155|157|160|156"] = true
 }
 
 -- Get the player's name
 local playerName = game.Players.LocalPlayer.Name
 
 -- Check if the player's name is in the whitelist
-local isWhitelisted = false
-for _, whitelistedName in ipairs(whitelist) do
-    if playerName == whitelistedName then
-        isWhitelisted = true
-        break
-    end
-end
+local isWhitelisted = whitelist[playerName] ~= nil
 
 -- If the player is not whitelisted, kick them
 if not isWhitelisted then
     game.Players.LocalPlayer:Kick("Not whitelisted")
-else
-    print("Whitelisted user")
 end
