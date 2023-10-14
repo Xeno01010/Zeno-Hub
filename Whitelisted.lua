0e9219ce203971c0fc8037cb3c09baf8c1e57636
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 16:12:24 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -7,35 +7,38 @@ local whitelist = {
     -- Add other whitelisted watermarks here
 }
 
-local Players = game:GetService("Players")
-local lp = game.Players.LocalPlayer
-
--- Function to generate watermark based on the UserId
-local function GenerateWatermark(UserId)
-    local WaterMark = "ZENO_HUB|"
-    local result = ""
-
-    -- Generate the watermark based on UserId
-    for i = 1, #tostring(UserId) do
-        local c = tostring(UserId):sub(i, i)
-        local k = ("didiask"):sub((i - 1) % #("didiask") + 1, (i - 1) % #("didiask") + 1)
-        result = result .. string.char((string.byte(c) + string.byte(k)) % 256)
+local function GetUserId(Value)
+    if not tonumber(Value) and Players:FindFirstChild(tostring(Value)) then
+        return Players:FindFirstChild(tostring(Value)).UserId
+    else
+        local Id = false
+        local SuccesId, ReturnName = pcall(function()
+            Players:GetNameFromUserIdAsync(Value)
+            Id = Value
+        end)
+        local SuccesName, ReturnId = pcall(function()
+            Id = Players:GetUserIdFromNameAsync(Value)
+        end)
+        return Id
     end
-
-    return WaterMark .. result:gsub(".", function(bb)
-        return "\\" .. string.byte(bb)
-    end) or "\\"
 end
 
--- Get the watermark for the current user
-local watermarkForCurrentUser = GenerateWatermark(lp.UserId)
+local lp = game.Players.LocalPlayer 
+local UserId = GetUserId(lp) 
 
--- Check if the generated watermark is in the whitelist
-local isWhitelisted = whitelist[watermarkForCurrentUser]
+local WaterMark  = "ZENO_HUB|"
+local result = ""
+for i = 1, #tostring(UserId) do
+local c = tostring(UserId):sub(i, i)
+local k = ("didiask"):sub((i - 1) % #("didiask") + 1, (i - 1) % #("didiask") + 1)
+result = result .. string.char((string.byte(c) + string.byte(k)) % 256)
+end
+local Input=WaterMark..result:gsub(".",(function(bb)
+return "\\"..string.byte(bb)
+end)) or "\\"
 
--- If the player is whitelisted, allow access; otherwise, kick them
-if isWhitelisted then
-    print("Access granted for player with UserId: " .. lp.UserId)
-else
-    lp:Kick("You are not whitelisted.")
+For _, wPly in ipairs(whitelist) do - - add list with ids
+if Input == wPly then
+return --LOAD SCRIPT HERE
+end
 end
