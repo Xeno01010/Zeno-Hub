b6a8816653cb95b5f373004f771ec589a99dad54
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 13:54:29 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -2,15 +2,19 @@ local whitelist = {
     ["ZENO_HUB|157|155|157|153|147|167|162|153|156"] = true,
     ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true,
     ["ZENO_HUB|101|106|100|112|105|124|109|106|107|101"] = true,
-    ["ZENO_HUB|151|153|151|160|146|171|155|157|160|156"] = true
+    ["ZENO_HUB|151|153|151|160|146|171|155|157|160|156"] = true,
+    ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true
 }
 
-local keyToCheck = "ZENO_HUB|157|155|157|153|147|167|162|153|156"
+-- Check if a key is in the whitelist
+local function isWhitelisted(key)
+    return whitelist[key] ~= nil
+end
 
-if whitelist[keyToCheck] then
-    print("Access granted for key: " .. keyToCheck)
+-- Example usage:
+local keyToCheck = "ZENO_HUB|149|154|148|160|153|172|157|154|155|149"
+if isWhitelisted(keyToCheck) then
+    print("Key is whitelisted")
 else
-    print("Access denied for key: " .. keyToCheck)
-    -- Kick the player
-    game.Players.LocalPlayer:Kick("Access denied for key: " .. keyToCheck)
+    print("Key is not whitelisted")
 end
