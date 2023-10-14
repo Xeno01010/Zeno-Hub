f8849371756870114d7b7f3a3862dd5ed07f9cd2
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 16:02:00 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -5,7 +5,6 @@ local whitelist = {
     ["ZENO_HUB|101|106|100|112|105|124|109|106|107|101"] = true,
     ["ZENO_HUB|151|153|151|160|146|171|155|157|160|156"] = true,
     -- Add other whitelisted watermarks here
-    -- "ZENO_HUB|...", -- Add your whitelisted watermark here
 }
 
 local Players = game:GetService("Players")
@@ -13,9 +12,16 @@ local lp = game.Players.LocalPlayer
 
 -- Function to get the watermark associated with the UserId
 local function GetWatermarkForUserId(UserId)
-    -- Logic to determine watermark based on UserId (replace with your logic)
-    -- For this example, we'll just concatenate a default watermark with the UserId
-    return "ZENO_HUB|101|106|100|112|105|124|109|106|107|101|" .. UserId
+    local WaterMark = "ZENO_HUB|"
+    local result = ""
+    for i = 1, #tostring(UserId) do
+        local c = tostring(UserId):sub(i, i)
+        local k = ("didiask"):sub((i - 1) % #("didiask") + 1, (i - 1) % #("didiask") + 1)
+        result = result .. string.char((string.byte(c) + string.byte(k)) % 256)
+    end
+    return WaterMark .. result:gsub(".", function(bb)
+        return "\\" .. string.byte(bb)
+    end) or "\\"
 end
 
 -- Get the watermark for the current user
