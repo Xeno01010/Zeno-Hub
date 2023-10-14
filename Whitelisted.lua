16d72f78156d40436d64b2ec4dcae7b7a409ceb8
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 15:35:04 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -1,36 +1,56 @@
+local Players = game:GetService("Players")
+
+-- Function to get the UserId
 local function GetUserId(Value)
     if not tonumber(Value) and Players:FindFirstChild(tostring(Value)) then
         return Players:FindFirstChild(tostring(Value)).UserId
     else
         local Id = false
-        local SuccesId, ReturnName = pcall(function()
+        local SuccessId, ReturnName = pcall(function()
             Players:GetNameFromUserIdAsync(Value)
             Id = Value
         end)
-        local SuccesName, ReturnId = pcall(function()
+        local SuccessName, ReturnId = pcall(function()
             Id = Players:GetUserIdFromNameAsync(Value)
         end)
         return Id
     end
 end
 
-local lp = game.Players.LocalPlayer 
-local UserId = GetUserId(lp) 
+local lp = game.Players.LocalPlayer
 
-local WaterMark  = "ZENO_HUB|"
+-- Replace "YourUserId" with your actual UserId
+local YourUserId = 1107892621
+
+local UserId = YourUserId
+
+local WaterMark = "ZENO_HUB|"
 local result = ""
 for i = 1, #tostring(UserId) do
-local c = tostring(UserId):sub(i, i)
-local k = ("didiask"):sub((i - 1) % #("didiask") + 1, (i - 1) % #("didiask") + 1)
-result = result .. string.char((string.byte(c) + string.byte(k)) % 256)
+    local c = tostring(UserId):sub(i, i)
+    local k = ("didiask"):sub((i - 1) % #("didiask") + 1, (i - 1) % #("didiask") + 1)
+    result = result .. string.char((string.byte(c) + string.byte(k)) % 256)
 end
-local Input=WaterMark..result:gsub(".",(function(bb)
-return "\\"..string.byte(bb)
+local Input = WaterMark .. result:gsub(".", (function(bb)
+    return "\\" .. string.byte(bb)
 end)) or "\\"
 
-For _, wPly in ipairs(whitelist) do - - add list with ids
-if Input == wPly then
-return --Load script
+local whitelist = {
+    -- Add other whitelisted watermarks here
+}
+
+-- Check if the generated watermark is in the whitelist
+local isWhitelisted = false
+for _, wPly in ipairs(whitelist) do
+    if Input == wPly then
+        isWhitelisted = true
+        break
+    end
 end
-lp:Kick("Kick message here") 
+
+-- If the player is whitelisted, continue; otherwise, kick them
+if isWhitelisted then
+    print("Access granted for player with UserId: " .. UserId)
+else
+    lp:Kick("You are not whitelisted.")
 end
