ddc69464a7aca3cd73efe7c7adf74baeeef37dbb
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 13:13:14 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -4,3 +4,23 @@ local whitelist = {
     ["ZENO_HUB|101|106|100|112|105|124|109|106|107|101"] = true,
     ["ZENO_HUB|151|153|151|160|146|171|155|157|160|156"] = true
 }
+
+local blacklist = {
+    ["SOME_KEY_TO_BLACKLIST"] = true,
+    -- Add other keys to blacklist as needed
+}
+
+function checkAccess(key)
+    if whitelist[key] then
+        print("Access granted for key: " .. key)
+    elseif blacklist[key] then
+        print("Access denied for key (blacklisted): " .. key)
+    else
+        print("Access denied for key (not in whitelist or blacklist): " .. key)
+    end
+end
+
+-- Testing access
+checkAccess("ZENO_HUB|157|155|157|153|147|167|162|153|156") -- Access granted
+checkAccess("SOME_KEY_TO_BLACKLIST") -- Access denied (blacklisted)
+checkAccess("SOME_OTHER_KEY") -- Access denied (not in whitelist or blacklist)
