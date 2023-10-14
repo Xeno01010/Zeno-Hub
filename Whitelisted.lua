f2d0faf784f83a88cbf118dc472dc3696a1932f8
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 15:44:25 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -1,19 +1,28 @@
-local Players = game:GetService("Players")
-
 -- Whitelist of watermarks
 local whitelist = {
-    "ZENO_HUB|157|155|157|153|147|167|162|153|156",
-    "ZENO_HUB|149|154|148|160|153|172|157|154|155|149",
-    "ZENO_HUB|101|106|100|112|105|124|109|106|107|101",
-    "ZENO_HUB|151|153|151|160|146|171|155|157|160|156"
+    ["ZENO_HUB|157|155|157|153|147|167|162|153|156"] = true,
+    ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true,
+    ["ZENO_HUB|101|106|100|112|105|124|109|106|107|101"] = true,
+    ["ZENO_HUB|151|153|151|160|146|171|155|157|160|156"] = true,
+    -- Add other whitelisted watermarks here
+    -- "ZENO_HUB|...", -- Add your whitelisted watermark here
 }
+
+local Players = game:GetService("Players")
 
 -- Function to get the UserId
 local function GetUserId(Value)
     if not tonumber(Value) and Players:FindFirstChild(tostring(Value)) then
         return Players:FindFirstChild(tostring(Value)).UserId
     else
-        local Id = Players:GetUserIdFromNameAsync(Value)
+        local Id = false
+        local SuccessId, ReturnName = pcall(function()
+            Players:GetNameFromUserIdAsync(Value)
+            Id = Value
+        end)
+        local SuccessName, ReturnId = pcall(function()
+            Id = Players:GetUserIdFromNameAsync(Value)
+        end)
         return Id
     end
 end
@@ -22,6 +31,7 @@ local lp = game.Players.LocalPlayer
 
 -- Replace "YourUserId" with your actual UserId
 local YourUserId = 1107892621
+
 local UserId = YourUserId
 
 local WaterMark = "ZENO_HUB|"
@@ -36,13 +46,7 @@ local Input = WaterMark .. result:gsub(".", (function(bb)
 end)) or "\\"
 
 -- Check if the generated watermark is in the whitelist
-local isWhitelisted = false
-for _, wPly in ipairs(whitelist) do
-    if Input == wPly then
-        isWhitelisted = true
-        break
-    end
-end
+local isWhitelisted = whitelist[Input]
 
 -- If the player is whitelisted, continue; otherwise, kick them
 if isWhitelisted then
