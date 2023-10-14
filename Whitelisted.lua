8cdbf1a6b1a4c975613d98bcfbfc39509bec4ef5
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 12:44:15 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -1,6 +1,5 @@
 local whitelist = {
     ["ZENO_HUB|157|155|157|153|147|167|162|153|156"] = true,
-    ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true,
     ["ZENO_HUB|101|106|100|112|105|124|109|106|107|101"] = true,
     ["ZENO_HUB|151|153|151|160|146|171|155|157|160|156"] = true,
     ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true
@@ -12,7 +11,7 @@ local function isWhitelisted(key)
 end
 
 -- Example usage:
-local keyToCheck = "ZENO_HUB|149|154|148|160|153|172|157|154|155|149"
+local keyToCheck =
 if isWhitelisted(keyToCheck) then
     print("Key is whitelisted")
 else
