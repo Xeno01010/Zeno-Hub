bacde4646e830284ab47671019b317c69ee5ae09
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 15:03:11 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -1,4 +1,3 @@
--- Whitelist based on UserIds
 local whitelist = {
     ["ZENO_HUB|157|155|157|153|147|167|162|153|156"] = true,
     ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true,
@@ -6,17 +5,3 @@ local whitelist = {
     ["ZENO_HUB|151|153|151|160|146|171|155|157|160|156"] = true,
     ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true
 }
-
--- Get the player's UserId and Name
-local playerId = tostring(game.Players.LocalPlayer.UserId)
-local playerName = game.Players.LocalPlayer.Name
-
--- Check if the player is whitelisted by UserId or Name
-local isWhitelisted = whitelist[playerId] or playerName == "ZENO_HUB|149|154|148|160|153|172|157|154|155|149"
-
--- If the player is not whitelisted, kick them
-if not isWhitelisted then
-    game.Players.LocalPlayer:Kick("Not whitelisted")
-else
-    print("Access granted for player: " .. playerName)
-end
