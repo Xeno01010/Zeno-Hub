9386505ef4b551dc727dc1c748968bc826414e59
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 14:19:53 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -2,15 +2,16 @@
 local whitelist = {
     ["ZENO_HUB|157|155|157|153|147|167|162|153|156"] = true,
     ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true,
-    ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true,
-    ["ZENO_HUB|151|153|151|160|146|171|155|157|160|156"] = true
+    ["ZENO_HUB|101|106|100|112|105|124|109|106|107|101"] = true,
+    ["ZENO_HUB|151|153|151|160|146|171|155|157|160|156"] = true,
+    ["ZENO_HUB|149|154|148|160|153|172|157|154|155|149"] = true
 }
 
--- Get the player's ID
-local playerId = game.Players.LocalPlayer.UserId
+-- Get the player's UserId
+local playerId = tostring(game.Players.LocalPlayer.UserId)
 
--- Check if the player's ID is in the whitelist
-local isWhitelisted = whitelist["ZENO_HUB|" .. playerId] == true
+-- Check if the player's UserId is in the whitelist
+local isWhitelisted = whitelist[playerId]
 
 -- If the player is not whitelisted, kick them
 if not isWhitelisted then
