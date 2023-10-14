35f5403536133808fe6203737425fcf2f962e13d
Xeno01010 <147944287+Xeno01010@users.noreply.github.com>
2023-10-14 12:53:02 -0700

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -1,3 +1,8 @@
+local IsWhitelisted = loadstring(game:HttpGet("https://raw.githubusercontent.com/H17S32/Other/main/WhitelistSystem_API.lua"))()
+if not IsWhitelisted then
+    return
+end
+
 -- Whitelist
 local whitelist = {
     ["ZENO_HUB|157|155|157|153|147|167|162|153|156"] = true,
@@ -12,26 +17,26 @@ local function isWhitelisted(key)
     return whitelist[key] ~= nil
 end
 
--- Example usage:
-local keyToCheck = "ZENO_HUB|149|154|148|160|153|172|157|154|155|149"
-if isWhitelisted(keyToCheck) then
-    print("Key is whitelisted")
-else
-    print("Key is not whitelisted")
-end
-
 -- Check if the player is whitelisted based on their UserId
 local function isPlayerWhitelisted(player)
     return player.UserId and whitelist["ZENO_HUB|" .. tostring(player.UserId)] == true
 end
 
--- Get the player
-local player = game.Players.LocalPlayer
+while true do
+    local player = game.Players.LocalPlayer
 
--- Check if the player is whitelisted
-if player and isPlayerWhitelisted(player) then
-    print("Player is whitelisted")
-else
-    print("Player is not whitelisted")
-    -- Optional: You can kick or handle blacklisted players here
+    -- Check if the player is whitelisted
+    if player and isPlayerWhitelisted(player) then
+        print("Player is whitelisted")
+        -- Run your script for whitelisted players here
+
+        -- For the purpose of this example, let's break the loop after one iteration
+        break
+    else
+        print("Player is not whitelisted")
+        -- Optional: You can kick or handle blacklisted players here
+    end
+
+    -- Delay before checking again (adjust as needed)
+    wait(5)
 end
